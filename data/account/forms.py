f8e107f024dc577b482cc58a9e12fc0f1a8d293d
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-12-10 02:01:55 +0200

    First epic: user permissions

---
+++ data/account/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+#user model
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+
+# allow users to edit their first name, last name, and email
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+#allow users to edit the profile data 
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
