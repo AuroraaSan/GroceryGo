6170bc75e426757947c6d044c6a8c5da9e9a27c9
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-12-11 21:48:11 +0200

    Added phone number field

--- account/admin.py
+++ account/admin.py
@@ -3,5 +3,5 @@ from .models import Profile
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'date_of_birth', 'photo']
+    list_display = ['user', 'date_of_birth', 'phone_number']
     raw_id_fields = ['user']
