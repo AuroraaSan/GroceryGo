38b573a953bb874274c64de6ad003fe1c20e67c9
mariambingoo <mariamsheta13@gmail.com>
2023-12-12 02:13:57 +0200

    Email linked with forget password

--- data/settings.py
+++ data/settings.py
@@ -143,7 +143,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'djangooservice@gmail.com'
-EMAIL_HOST_PASSWORD = 'exwq lgdj shuf vjgz'
+EMAIL_HOST_PASSWORD = '   '
 
 
 # Whether to send SMTP 'Date' header in the local time zone or in UTC.
