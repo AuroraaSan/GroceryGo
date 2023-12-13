9a7f8602a9cecc4f8ac2d249a141a25161906889
Ahmed <a.hagagabdallah@gmail.com>
2023-12-14 01:19:49 +0200

    Merged with Orders + fixed multiple orders when refereshed bug

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'orders'
+app_name = "orders"
 
 urlpatterns = [
-    path('create/', views.order_create, name='order_create'),
+    path("create/", views.order_create, name="order_create"),
+    path("order/created/<int:order_id>/", views.order_created, name="order_created"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,29 +1,31 @@
-from django.shortcuts import render
-from .models import OrderItem
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
-from cart.cart import Cart
+from cart.cart import CartWrapper
 
 
 def order_create(request):
-    cart = Cart(request)
-    if request.method == 'POST':
+    cart = CartWrapper(request.user)
+    if request.method == "POST":
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
             for item in cart:
-                OrderItem.objects.create(order=order,
-                                        product=item['product'],
-                                        price=item['price'],
-                                        quantity=item['quantity'])
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    price=item["price"],
+                    quantity=item["quantity"],
+                )
             # clear the cart
             cart.clear()
-            # launch asynchronous task
-    
-            return render(request,
-                          'orders/order/created.html',
-                          {'order': order})
+
+            return redirect("orders:order_created", order_id=order.id)
     else:
         form = OrderCreateForm()
-    return render(request,
-                  'orders/order/create.html',
-                  {'cart': cart, 'form': form})
+    return render(request, "orders/order/create.html", {"cart": cart, "form": form})
+
+
+def order_created(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, "orders/order/created.html", {"order": order})
