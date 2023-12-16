ec507897831b9789a079dbae5fe3597c75433bdd
mariambingoo <mariamsheta13@gmail.com>
2023-12-17 01:34:58 +0200

    comments removed

--- orders/urls.py
+++ orders/urls.py
@@ -6,6 +6,6 @@ app_name = "orders"
 urlpatterns = [
     path("create/", views.order_create, name="order_create"),
     path("order/created/<int:order_id>/", views.order_created, name="order_created"),
-    #path('admin/order/<int:order_id>/pdf/',views.admin_order_pdf,name='admin_order_pdf'),
+    path('admin/order/<int:order_id>/pdf/',views.admin_order_pdf,name='admin_order_pdf'),
 
 ]
