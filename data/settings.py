c88936238c061661f2ea887341dd541f323a92d2
youssef-elharty <youssefashraf246@gmail.com>
2023-12-23 02:55:01 +0200

    Fixed: pagination keeps query parameters

--- data/settings.py
+++ data/settings.py
@@ -89,7 +89,8 @@ WSGI_APPLICATION = "data.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
+# DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
+DATABASES = {"default": env.dj_db_url('sqlite:///db.sqlite3')}
 
 
 # Password validation
